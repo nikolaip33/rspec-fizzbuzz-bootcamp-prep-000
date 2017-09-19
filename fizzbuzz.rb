dbcfa9984db42c298a2fde3b667bd6cab2284ba9
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-19 19:45:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(number)
+  if number%3 == 0 && number%5 == 0
+    "FizzBuzz"
+  end
   if number%3 == 0
     "Fizz"
   end
   if number%5 == 0
     "Buzz"
   end
-  if number%3 == 0 && number%5 == 0
-    "FizzBuzz"
-  end
+  
 end
